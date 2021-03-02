2af9d5a148c3269a2d532efe3c9bb20a689ff2c1
yuuki1293 <yuuki1293@outlook.jp>
2021-03-02 15:18:38 +0900

    タイトルバーの表示の読み取りに成功した

--- マイメモ帳/Data.cs
+++ マイメモ帳/Data.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -12,8 +14,8 @@ namespace マイメモ帳
     public class Data
     {
         internal Font TextFont { get; set; }
-
-        public bool ShowTitleBar { get; set; }
+        internal bool ShowTitleBar { get; set; }
+        internal Dictionary<string,Color> Colors { get; set; }
 
         internal void Save()
         {
@@ -29,45 +31,9 @@ namespace マイメモ帳
 
         internal void Load()
         {
-            //var xmlSerializer = new XmlSerializer(typeof(Data));
             var path = Path.Combine(Application.UserAppDataPath, "usersetting.xml");
             XElement xml = XElement.Load(path);
-            var infos = from item in xml.Elements("ShowTitleBar")
-                select item;
-
-            //要素分ループして、コンソールに表示
-            foreach (var info in infos)
-            {
-                MessageBox.Show(info.Value);
-            }
-
-            // if (!File.Exists(path)) { return new Data(); }
-            //
-            // var settings = new XmlReaderSettings { CheckCharacters = false };
-            // using (var streamReader = new StreamReader(path,Encoding.UTF8)) 
-            // using (var xmlReader = XmlReader.Create(streamReader,settings))
-            // {
-            //     Data data =(Data)xmlSerializer.Deserialize(xmlReader);
-            //     streamReader.Close();
-            //     return data;
-            // }
-        }
-
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        [Browsable(false)]
-        public string DisplayFontAsString
-        {
-            get => ConvertToString(TextFont);
-            set => TextFont = ConvertFromString<Font>(value);
-        }
-
-        public static string ConvertToString<T>(T value)
-        {
-            return TypeDescriptor.GetConverter(typeof(T)).ConvertToString(value);
-        }
-        public static T ConvertFromString<T>(string value)
-        {
-            return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(value);
+            ShowTitleBar =Convert.ToBoolean(xml.Elements("ShowTitleBar").Single().Value);
         }
     }
 }
