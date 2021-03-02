cb93f0b0fc387e76d11764f365457a30e9a00787
yuuki1293 <yuuki1293@outlook.jp>
2021-03-02 22:05:25 +0900

    要素がなくてもLoadできるようになった

--- マイメモ帳/Data.cs
+++ マイメモ帳/Data.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using System.Xml.Serialization;
 
 namespace マイメモ帳
 {
@@ -28,30 +27,68 @@ namespace マイメモ帳
         internal void Load()
         {
             var path = Path.Combine(Application.UserAppDataPath, "usersetting.xml");
+            if (!File.Exists(path))
+            {
 
+            }
             var xml = XElement.Load(path);
-            ShowTitleBar = Convert.ToBoolean(xml.Elements("ShowTitleBar").Single().Value);
-            var loadFont = xml.Elements("Font");
-            var xElements = loadFont as XElement[] ?? loadFont.ToArray();
-            var bold = Convert.ToBoolean(xElements.Elements("Bold").Single().Value);
-            var italic = Convert.ToBoolean(xElements.Elements("Italic").Single().Value);
-            TextFont = new Font
+            ShowTitleBar = Convert.ToBoolean(NodeExists(xml,"ShowTitleBar","true"));
+            try
+            {
+                var loadFont = xml.Elements("Font"); 
+                var xElements = loadFont as XElement[] ?? loadFont.ToArray();
+                var bold = Convert.ToBoolean(xElements.Elements("Bold").Single().Value);
+                var italic = Convert.ToBoolean(xElements.Elements("Italic").Single().Value);
+                TextFont = new Font
                 (
                     xElements.Elements("Name").Single().Value,
                     int.Parse(xElements.Elements("Size").Single().Value),
                     bold && italic ? (FontStyle.Bold | FontStyle.Italic) :
-                            bold ? FontStyle.Bold :
-                            italic ? FontStyle.Italic :
-                            FontStyle.Regular
+                    bold ? FontStyle.Bold :
+                    italic ? FontStyle.Italic :
+                    FontStyle.Regular
                 );
+            }
+            catch (System.InvalidOperationException)
+            {
+                xml.Elements("Font").Remove();
+                xml.Add(new XElement("Font",
+                    new XElement("Name","MS UI Gothic"),
+                    new XElement("Size",12),
+                    new XElement("Bold",false),
+                    new XElement("Italic",false)
+                ));
+                TextFont = new Font("MS UI Gothic",12);
+            }
+            
+            if (!xml.Elements("Color").Any())xml.Add(new XElement("Color"));
             var colors = xml.Elements("Color");
             Colors = new Dictionary<string, Color>();
             foreach (var color in colors)
             {
                 var key = color.FirstAttribute.Value;
-                var value = Color.FromArgb(Convert.ToInt32(color.Value,16));
-                Colors.Add(key,value);
+                var value = Color.FromArgb(Convert.ToInt32(color.Value, 16));
+                Colors.Add(key, value);
             }
+            xml.Save(path);
+        }
+
+        private string NodeExists(XElement xml, string nodeName, string defaultValue = null)
+        {
+            try
+            {
+                return xml.Elements(nodeName).Single().Value;
+            }
+            catch (System.InvalidOperationException)
+            {
+                xml.Add(new XElement(nodeName, defaultValue));
+                return defaultValue;
+            }
+        }
+
+        private void SettingReset(string path)
+        {
+            File.Create(path);
         }
     }
 }
